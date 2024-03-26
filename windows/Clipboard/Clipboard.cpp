56e08564c8f97772ac232c5779af512c6cc779e0
Roland Zechner <roland@zechner.it>
2024-03-26 11:31:37 +0100

    Refactor SetHtmlAndString method to add missing HTML and body tags

--- windows/Clipboard/Clipboard.cpp
+++ windows/Clipboard/Clipboard.cpp
@@ -48,33 +48,56 @@ namespace NativeClipboard {
 
   void ClipboardModule::SetHtmlAndString(std::string const& html, std::string const& str) noexcept {
     _context.UIDispatcher().Post([html, str](){
-      std::string cfHtml = "";
+      std::string cfHtml = html;
 
-      if (html.find("Version:") == 0) {
-        cfHtml = html;
-      } else {
-        std::string startFragmentStr = "<!--StartFragment-->";
-        std::string endFragmentStr = "<!--EndFragment-->";
-        std::string htmlContent = "<html>" + startFragmentStr + "<body>" + html + "</body>" + endFragmentStr + "</html>";
-
-        cfHtml = 
-          "Version:1.0\r\n"
-          "StartHTML:0000000000\r\n"
-          "EndHTML:0000000000\r\n"
-          "StartFragment:0000000000\r\n"
-          "EndFragment:0000000000\r\n"
-          + htmlContent;
-
-        int startHtml = cfHtml.find("<html>");
-        int endHtml = cfHtml.find("</html>") + 7;
-        int startFragment = cfHtml.find(startFragmentStr) + startFragmentStr.length();
-        int endFragment = cfHtml.find(endFragmentStr);
-
-        cfHtml.replace(cfHtml.find("StartHTML:0000000000"), 20, "StartHTML:" + std::to_string(startHtml).insert(0, 10 - std::to_string(startHtml).length(), '0'));
-        cfHtml.replace(cfHtml.find("EndHTML:0000000000"), 18, "EndHTML:" + std::to_string(endHtml).insert(0, 10 - std::to_string(endHtml).length(), '0'));
-        cfHtml.replace(cfHtml.find("StartFragment:0000000000"), 24, "StartFragment:" + std::to_string(startFragment).insert(0, 10 - std::to_string(startFragment).length(), '0'));
-        cfHtml.replace(cfHtml.find("EndFragment:0000000000"), 22, "EndFragment:" + std::to_string(endFragment).insert(0, 10 - std::to_string(endFragment).length(), '0'));
+      if (cfHtml.find("Version:") == 0) {
+        for (int i = 0; i < 5; ++i) {
+          size_t pos = cfHtml.find('\n');
+          if (pos != std::string::npos) {
+              cfHtml.erase(0, pos + 1);
+          } else {
+              break;
+          }
+        }
       }
+
+      std::string startFragmentStr = "<!--StartFragment-->";
+      std::string endFragmentStr = "<!--EndFragment-->";
+      std::string bodyStartStr = "<body";
+      std::string bodyEndStr = "</body>";
+      std::string htmlStartStr = "<html";
+      std::string htmlEndStr = "</html>";
+
+      if (cfHtml.find(startFragmentStr) == std::string::npos || cfHtml.find(endFragmentStr) == std::string::npos) {
+          cfHtml = startFragmentStr + cfHtml + endFragmentStr;
+      }
+
+      if (cfHtml.find(bodyStartStr) == std::string::npos || cfHtml.find(bodyEndStr) == std::string::npos) {
+          cfHtml = bodyStartStr+'>' + cfHtml + bodyEndStr;
+      }
+      
+      if (cfHtml.find(htmlStartStr) == std::string::npos || cfHtml.find(htmlEndStr) == std::string::npos) {
+          cfHtml = htmlStartStr+'>' + cfHtml + htmlEndStr;
+      }
+
+      cfHtml = 
+        "Version:1.0\r\n"
+        "StartHTML:0000000000\r\n"
+        "EndHTML:0000000000\r\n"
+        "StartFragment:0000000000\r\n"
+        "EndFragment:0000000000\r\n"
+        + cfHtml;
+
+      int startHtml = cfHtml.find(htmlStartStr);
+      int endHtml = cfHtml.find(htmlEndStr) + 7;
+      int startFragment = cfHtml.find(startFragmentStr) + startFragmentStr.length();
+      int endFragment = cfHtml.find(endFragmentStr);
+
+      cfHtml.replace(cfHtml.find("StartHTML:0000000000"), 20, "StartHTML:" + std::to_string(startHtml).insert(0, 10 - std::to_string(startHtml).length(), '0'));
+      cfHtml.replace(cfHtml.find("EndHTML:0000000000"), 18, "EndHTML:" + std::to_string(endHtml).insert(0, 10 - std::to_string(endHtml).length(), '0'));
+      cfHtml.replace(cfHtml.find("StartFragment:0000000000"), 24, "StartFragment:" + std::to_string(startFragment).insert(0, 10 - std::to_string(startFragment).length(), '0'));
+      cfHtml.replace(cfHtml.find("EndFragment:0000000000"), 22, "EndFragment:" + std::to_string(endFragment).insert(0, 10 - std::to_string(endFragment).length(), '0'));
+
       auto dataPackage = datatransfer::DataPackage();
       dataPackage.SetHtmlFormat(winrt::to_hstring(cfHtml));
       if (!str.empty()) {
